95947c8746aa45d5e5593dcc95429bcae0bcb928
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-09-15 00:08:28 +0900

    Dialogのエラーを修正。コンテンツ内容を追記。

--- src/components/Charactor.tsx
+++ src/components/Charactor.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import Grid from '@material-ui/core/Grid';
@@ -9,18 +9,44 @@ import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
-import InputStyles from './InputStyles';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
-      padding: theme.spacing(3)
+      padding: theme.spacing(3),
     },
     button: {
       padding: theme.spacing(1),
       textAlign: 'center',
+      fontWeight: 600,
+      marginBottom: '8px',
       color: theme.palette.text.primary,
+      '&:hover': {
+        backgroundColor: '#ffff',
+      },
+    },
+    whiteKuuga: {
+      borderColor: '##ff6768',
+      color: '##ff6768',
+    },
+    redKuuga: {
+      borderColor: '#dd0a35',
+      color: '#dd0a35',
+    },
+    blueKuuga: {
+      borderColor: '#003399',
+      color: '#003399',
+    },
+    greenKuuga: {
+      borderColor: '#2BDE73',
+      color: '#2BDE73',
+    },
+    perpleKuuga: {
+      borderColor: '#791E94',
+      color: '#791E94',
     },
   })
 );
@@ -32,11 +58,17 @@ interface Props {
   onClose: () => void;
 }
 
-const DialogTitle = (props: Props) => {
-  const { onClose } = props;
+interface DialogProps {
+  id: string;
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const DialogTitle = (props: DialogProps) => {
+  const { children, onClose } = props;
   return (
-    <MuiDialogTitle disableTypography>
-      <Typography variant='button'></Typography>
+    <MuiDialogTitle>
+      <Typography variant='button'>{children}</Typography>
       {onClose ? (
         <IconButton aria-label='close' onClick={onClose}>
           <CloseIcon />
@@ -50,30 +82,42 @@ export const Charactor: React.FC<Props> = ({forms}) => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
 
-  const handleClickOpen = () => {
+  const clickDialogOpen = () => {
     setOpen(true);
   };
-  const handleClose = () => {
+  const clickDialogClose = () => {
     setOpen(false);
   };
 
     return (
       <div className={classes.root}>
-        <Grid container spacing={3}></Grid>
+        <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
             {forms.map((form) => (
-              <Button variant='outlined' color='secondary' onClick={handleClickOpen}>
+              <Button className={classes.button} size='large' variant='outlined' color='secondary' onClick={clickDialogOpen}>
                 {form}
               </Button>
             ))}
           </Grid>
-          <Dialog onClose={handleClose} aria-labelledby='customized-dialog-title' open={open}>
-            戦士クウガ詳細情報
-            <Button autoFocus onClick={handleClose} color='primary'>
+          <Dialog onClose={clickDialogClose} aria-labelledby='customized-dialog-title'open={open}>
+            <DialogTitle id="customized-dialog-title" onClose={clickDialogClose}>
+              詳細情報
+            </DialogTitle>
+            <DialogContent dividers>
+              <Typography variant="subtitle1">
+                パンチ力：
+              </Typography>
+              <Typography variant="subtitle1">
+                キック力：
+              </Typography>
+            </DialogContent>
+            <DialogActions>
+              <Button autoFocus onClick={clickDialogClose} color='primary'>
               閉じる
-            </Button>
+              </Button>
+            </DialogActions>
           </Dialog>
-        <Grid />
+        </Grid>
       </div>
     );
 };

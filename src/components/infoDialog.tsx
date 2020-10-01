ba6c740616ab3b867e4859533d114fa67579b0cd
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-10-01 23:56:57 +0900

    コンポーネントの切り出し

--- src/components/infoDialog.tsx
+++ src/components/infoDialog.tsx
@@ -1,1 +1,12 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const TypographyContent = (props:any) => {
+  <Typography
+    variant={props.variant}
+
+  />
+
+}
+
+export default TypographyContent;
